300971e55778431d966533c0bb6dbe190e03197a
r4 <r4@nobrain.org>
2022-04-11 19:22:53 +0200

    improve documentation

--- spybug/spybug.ino
+++ spybug/spybug.ino
@@ -1,14 +1,28 @@
-/* Wiring:
+/*
+	*** SD Card Wiring ***
 	SD       | Nano
 	______________________
 	D0  (DO) |  D12 (MISO)
 	VSS      |  GND
 	CLK      |  D13 (SCK)
-	VDD      |  3V3
+	VDD      |  5V or 3V3
 	CMD (DI) |  D11 (MOSI)
 	D3  (CS) |  D10 (SS)
+
+	WARNING: SD cards are not designed for 5V; I have been using 5V anyways
+	and everything seems fine, but beware that there is a significant risk
+	of immediate or premature failure when not using a buffer circuit.
 	
-	SD pin D3 is the chip select pin (must be set manually in PIN_SS).
+	SD pin D3 is the chip select pin. It can be set manually in PIN_SS.
+
+	*** Microphone Wiring (MAX9814 w/ electret microphone) ***
+	Mic      | Nano
+	______________________
+	VCC      |  5V
+	GND      |  GND
+	Out      |  A0
+	
+	Out defaults to A0 (AdcChannel0), but can be set manually in ADC_CHANNEL.
 */
 
 #include <SD.h>
